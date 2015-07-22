221283a73087a9aaca9a719af60617bc3b3f42e3
hiroshings <hiroshings.job1987@gmail.com>
2015-07-22 23:23:11 +0900

    D004:文字列の結合

--- d015.php
+++ d015.php
@@ -1,6 +1,6 @@
 <?php
 	$inputNumber = fgets(STDIN);
 	//$inputNumberから１までカウントダウン
-	for($i = 0; $i < $inputNumber; $i++) {
+	for ($i = 0; $i < $inputNumber; $i++) {
 		echo $inputNumber - $i ."\n";
 	}
