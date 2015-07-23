8c7fa58b2847988b9f0946d7b5d66bd05f196679
hiroshings <hiroshings.job1987@gmail.com>
2015-07-23 23:36:12 +0900

    コードレビューの修正。初回の[$i] = fgets(STDIN);は余計

--- d017.php
+++ d017.php
@@ -2,7 +2,7 @@
 	//5つある数字の最大・最小を求める
 	define("COUNT" , 5);
 	for($i = 0; $i < COUNT; $i++) {
-		$inputNumbers[$i] = fgets(STDIN);
+		$inputNumbers[] = fgets(STDIN);
 		$inputNumbers[$i] = (int)$inputNumbers[$i];
 		//int型の変数にstr型の値が上書きされるので下記はNG
 		//(int)$inputNumbers[$i] = fgets(STDIN);
