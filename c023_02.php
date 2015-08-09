1f49f250b2de0821338c4940284a30de31f9bf49
hiroshings <hiroshings.job1987@gmail.com>
2015-08-09 23:15:46 +0900

    c023レビュー + 課題　の　解答

--- c023_02.php
+++ c023_02.php
@@ -8,12 +8,6 @@ for ($i = 0; $i < $tickets; $i++) {
 	displayHitCount($hitNumbers, $ticketNumbers);
 }
 
-/************************
- * 課題
- * 下記のメソッドを完成させて呼び出して
- * このプログラムの別解をだしてください
- ***********************/
-
 /**
  * 当選数表示関数
  *
@@ -28,21 +22,13 @@ for ($i = 0; $i < $tickets; $i++) {
 function displayHitCount($hitNumbers, $ticketNumbers) {
 	$matchCount = 0;
 	foreach ($hitNumbers as $hitNumber) {
-		// 仕様変更があって「当選番号の数が10個になりました」
-		// ってときにここ直す必要が出てくる
-		// つまり仕様変更に弱い書き方
-		// foreachを使わなかった理由はなに？
 		foreach ($ticketNumbers as $ticketNumber) {
 			if ($hitNumber === $ticketNumber) {
 				$matchCount++;
-				break;
+				break;//マッチしたら、ループを抜ける
 			}
-			// ここも効率が悪い
-			// クジなので、同じ数字は2回出てこないはず
-			// ヒットした時点でループを抜けないと、残りの処理分のループがムダ
 		}
 	}
-	// ここでechoすりゃよくね？
 	echo $matchCount . "\n";
 }
 
