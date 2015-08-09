0bbb09bb0f3a4c534de3e3e4aa296fae4f113e74
hiroshings <hiroshings.job1987@gmail.com>
2015-08-09 23:11:52 +0900

    c023レビュー + 課題　の　解答

--- c023_02.php
+++ c023_02.php
@@ -1,36 +1,12 @@
 <?php
-$hitNums = getLineDatasSplitedBySpace(fgets(STDIN));//当選番号
+$hitNumbers = getLineDatasSplitedBySpace(fgets(STDIN));//当選番号
 $tickets = trim(fgets(STDIN));//チケットの枚数
-$matchCounts = [];
 
-//当選番号とチケット番号の比較
+//当選した数の表示
 for ($i = 0; $i < $tickets; $i++) {
-	$matchCount = 0;
-	$ticketNums = getLineDatasSplitedBySpace(fgets(STDIN));
-	//当選番号とチケット番号がマッチしたら、カウントする
-	foreach ($hitNums as $hitNum) {
-		// 仕様変更があって「当選番号の数が10個になりました」
-		// ってときにここ直す必要が出てくる
-		// つまり仕様変更に弱い書き方
-		// foreachを使わなかった理由はなに？ 
-		for ($j = 0; $j < 6; $j++) {
-			if ($hitNum === $ticketNums[$j]) {
-				$matchCount++;
-			}
-			// ここも効率が悪い
-			// クジなので、同じ数字は2回出てこないはず
-			// ヒットした時点でループを抜けないと、残りの処理分のループがムダ
-		}
-	}
-	// ここでechoすりゃよくね？
-	$matchCounts[$i] = $matchCount;
+	$ticketNumbers = getLineDatasSplitedBySpace(fgets(STDIN));
+	displayHitCount($hitNumbers, $ticketNumbers);
 }
-//チケットごとのマッチした数を出力
-foreach ($matchCounts as $matchCount) {
-	echo $matchCount . "\n";
-}
-
-
 
 /************************
  * 課題
@@ -40,21 +16,35 @@ foreach ($matchCounts as $matchCount) {
 
 /**
  * 当選数表示関数
- * 
+ *
  * 引数で受け取った自分のクジの番号の配列と当選番号の配列を基に
  * 当たった番号の数を表示する
  *
  * @param array $hitNumbers
- *				当選番号の配列	
+ *				当選番号の配列
  * @param array $ticketNumbers
  * 				自分のクジ番号の配列
  **/
 function displayHitCount($hitNumbers, $ticketNumbers) {
-
+	$matchCount = 0;
+	foreach ($hitNumbers as $hitNumber) {
+		// 仕様変更があって「当選番号の数が10個になりました」
+		// ってときにここ直す必要が出てくる
+		// つまり仕様変更に弱い書き方
+		// foreachを使わなかった理由はなに？
+		foreach ($ticketNumbers as $ticketNumber) {
+			if ($hitNumber === $ticketNumber) {
+				$matchCount++;
+				break;
+			}
+			// ここも効率が悪い
+			// クジなので、同じ数字は2回出てこないはず
+			// ヒットした時点でループを抜けないと、残りの処理分のループがムダ
+		}
+	}
+	// ここでechoすりゃよくね？
+	echo $matchCount . "\n";
 }
-
-
-
 
 //空白で区切られたデータを配列で返却する
 function getLineDatasSplitedBySpace($lineData) {
