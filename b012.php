512d5cc0693df2703c327fe18b03bb3bf20cbc08
maeshun <butterflife2007@gmail.com>
2015-10-07 10:51:32 +0900

    review

--- b012.php
+++ b012.php
@@ -7,15 +7,24 @@ for ($i = 0; $i < $numberOfCards; $i++) {
 	$cardNums = str_split($cardNums);
 	$cardNums = array_reverse($cardNums);
 
+	// イマイチなにをしたい関数なのかが関数名から見えてこない
+	// singleDiditってなに？
 	outputSingleDidit($cardNums);
 }
 
+// 関数が多機能すぎる印象
+// 値を計算して返すくらいにしておいたほうがよい
+// 後々計算は使うけど、表示はしないような利用ケースが来た場合困る
 
 //カード番号１桁目を出力
 function outputSingleDidit($cardNums) {
 	//偶数桁・奇数桁の配列を取得
 	for ($i = 1; $i < count($cardNums); $i++) {
 		if ($i % 2 === 0) {
+			// あとでarray_sumやるくらいならここで総和計算すれば良くない？
+			// 全部の値まわしてるんだから
+			// あと、$i%2===0なのに代入する変数が$oddsなのはなにも知らずに後から見た場合混乱する
+			// 一言説明を書いておくべき
 			$odds[] = $cardNums[$i];
 		} else {
 			$evens[] = $cardNums[$i];
@@ -27,10 +36,14 @@ function outputSingleDidit($cardNums) {
 
 	//偶数桁の総和を取得
 	foreach ($evens as $even) {
+		// foreachのループしてる変数の代入し直しはしない方が良い
+		// このループがなんのループかがわからなくなる
 		$even = $even * 2;
 		if ($even >= 10) {
+			// ここもコメントが欲しい。10以上のときはなにやってるの？
 			$even = substr($even, 0, 1) + substr($even, 1, 1);
 		}
+		// doubledEvensじゃない？
 		$evensDoubled[] = $even;
 	}
 	$totalEven = array_sum($evensDoubled);
@@ -38,6 +51,8 @@ function outputSingleDidit($cardNums) {
 	$total = $totalOdd + $totalEven;
 
 	//10で割り切れる1桁目(X)を出力
+	// 10で割ってどうのこうのって書いてあるのに、わざわざn桁目を足してどうのこうのってやるからバグる
+	// 10で割った余りをどうするかって考えれば良いじゃない
 	$x = 10 - substr($total, -1, 1);
 	if ($x === 10) {
 		echo 0;
