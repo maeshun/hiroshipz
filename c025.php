b4d06dbbb516273c6c05a3936540159d409ccbda
maeshun <butterflife2007@gmail.com>
2016-03-01 13:43:46 +0900

    c025レビュー

--- c025.php
+++ c025.php
@@ -1,4 +1,5 @@
 <?php
+// can〜はイケてない。〜ableにしよう。
 $canCarryPaper = trim(fgets(STDIN)); // 運べる枚数
 $faxCount = trim(fgets(STDIN)); // FAXが届く回数
 
@@ -9,23 +10,32 @@ $count = 0;
 
 // 1時間毎の合計枚数を保持した配列を作成
 for ($i = 0; $i < $faxCount; $i++) {
+	// ここで$faxDataを溜め込む理由ってなに？
+	// ループの外で利用していないので上書きで良いのでは？
 	$faxData[] = getLineDataSplitedByDelimiter(fgets(STDIN), " ");
 	$hour = $faxData[$i][0];
 	$numOfPapers = $faxData[$i][2];
 
+	// ここから下の7行が冗長
+	// この1行で解決する
+	// $totalNumOfPapers[$faxData[$i][0]] += $numOfPapers;
 	if ($preHour < $hour) {
 		$preHour = $hour;
 		$count++;
 	}
 
+	// 上のifがあるんだから、ここは必ず通るんだよね？
+	// 無駄判定
 	if ($preHour == $hour) {
+		// totalNumOfPapersっていう変数名だと、単純に枚数のみが格納されているように見える
 		$totalNumOfPapers[$count] += $numOfPapers;
 	}
+
 }
 
 // 1時間ごとに運ぶ回数を計算し、合計で運ぶ回数を出力
 foreach ($totalNumOfPapers as $papersAnHour) {
-
+	// papersAnHourって変数名がイケてない
 	$totalCarryCount += ceil($papersAnHour / $canCarryPaper);
 }
 
